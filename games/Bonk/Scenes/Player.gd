f86a1d33282aa2d83ba1c9410cbec78149186580
Mdl74fsre1hz <51497263+4487R9cH03FObS@users.noreply.github.com>
2021-07-14 18:29:20 -0400

    Avances

--- games/Bonk/Scenes/Player.gd
+++ games/Bonk/Scenes/Player.gd
@@ -7,11 +7,16 @@ var move_down = "move_down"
 var action_a = "action_a"
 var action_b = "action_b"
 
+var id :int 
+
+
 var bone_material : SpatialMaterial = SpatialMaterial.new()
 
-var player 
+var player
+var player_id = 4
+var index
 
-func init(_player: Player):
+func init(_player: Player,id,index):
 	# Esta línea hará que el objeto sepa quién es su "dueño"
 	set_network_master(_player.nid)
 	player=_player
@@ -24,6 +29,8 @@ func init(_player: Player):
 	move_down = "move_down_" + ks
 	action_a = "action_a_" + ks
 	action_b = "action_b_" + ks
+	player_id = id
+	var __ = connect("notify_bigness",index,"_on_notify_bigness")
 	
 	bone_material.albedo_color = player.color
 	
@@ -36,15 +43,25 @@ func init(_player: Player):
 	# También puedes guardar player en una
 	# variable para acceder a ella después
 
+var frozen : bool = true
+func _on_freeze():
+	frozen = true
+	pass
+
+func _on_unfreeze():
+	frozen = false
+	pass
+
 func printsk(string):
 	print("skeleton ",player.nid," : ",string)
 
 func _input_bigger(input : String) -> void:
 	if Input.is_action_just_pressed(input):
-		_make_bigger()
-		rpc("_make_bigger")
+		rpc("_make_bigger",player_id)
 
 func _process(delta):
+	if frozen:
+		return
 	if is_network_master():
 		_input_bigger(move_down)
 		_input_bigger(move_left)
@@ -61,9 +78,10 @@ const biggyness_increment : float = 1.0
 func _scale(bigg : float) -> float:
 	return pow(bigg,0.333)
 
-puppet func _make_bigger():
+signal notify_bigness
+
+remotesync func _make_bigger(whom: int):
 	biggyness+=biggyness_increment
-	#printsk("big")
-	#print(biggyness)
+	emit_signal("notify_bigness",whom,biggyness)
 
 
--- games/Bonk/index.gd
+++ games/Bonk/index.gd
@@ -6,25 +6,45 @@ var players
 var player_scores = [] #{"player": player, "points": score}
 onready var camera : Camera = $CameraContainer/Camera
 
+signal unfreeze
+signal freeze
+
 func _ready():
 	camera.make_current()
 	
 	players = Party.get_players().duplicate()
 	for i in range(players.size()):
 		var p = player_scene.instance()
-		p.init(players[i])
-		p.global_transform.origin = $PositionContainer.get_children()[i].global_transform.origin
+		p.init(players[i],i,self)
+		p.global_transform = $PositionContainer.get_children()[i].global_transform
 		$PlayerContainer.add_child(p)
-
+		catch_connection(connect("freeze",p,"_on_freeze"))
+		catch_connection(connect("unfreeze",p,"_on_unfreeze"))
+	
+	if is_network_master():
+		_setup_cycle_timer()
+		_cycle()
 
 func _process(delta):
 	_get_bigness()
+	_lambda_update()
 	_vector_update()
-	_camera_update()
+	if _cycle_var!=_cycles.entrance:
+		_camera_update()
+	#print(cycle_timer.time_left)
 
-func _get_bigness():
+var bigness_array : PoolRealArray = [1.0,1.0,1.0,1.0,0.0]
+
+func _get_bigness(): # ?? parece que desincronnizó
 	for child in $PlayerContainer.get_children():
 		max_big = max(max_big,child.biggyness)
+	#for i in range(players.size()):
+	#	max_big = max(max_big,child.biggyness)
+
+func _on_notify_bigness(whom:int,howmuch:float):
+	bigness_array[whom] = howmuch
+
+################## Manejo de la cámara #########################################
 
 var max_big : float =1.0
 onready var start_pos : Vector3 = $CameraContainer/Start.global_transform.origin
@@ -41,37 +61,153 @@ func _camera_update():
 var pos_lambda : float = 0.0
 var aim_lambda : float = 0.0
 
-export var reasonable_key_presses : float = 1000.0
+export var play_time : float = 20.0
+export var result_time : float = 5.0
+export var reasonable_key_presses : float = 1000.0 # TODO: ESTIMATE THIS
+
 func _lambda_update():
 	pos_lambda = min(pow((2*max_big-1),1.1)/pow(reasonable_key_presses,1.1),1)
 	aim_lambda = min(pow((2*max_big-1),1.1)/pow(reasonable_key_presses,1.1),1)
 
 func _vector_update():
-	_lambda_update()
 	target_pos =  (1-pos_lambda)*start_pos  + pos_lambda*end_pos
 	target_aim =  (1-aim_lambda)*start_aim  + aim_lambda*end_aim
 
-var _cycle_var : int =0
+func _override_camera_pos(pos):
+	target_pos = pos
+
+############## Manejo de Ciclos ##################
+
+var _cycle_var : int = _cycles.entrance
 enum _cycles {entrance,countdown,start,game,stop,end,exit}
 
+var ended : bool = false
 var results : Array
 func _cycle():
+	# obs, esto solo se ejecuta desde master!
+	if ended:
+		return
 	match _cycle_var:
 		_cycles.entrance:
-			pass
+			printg(["entrance"])
+			cycle_timer.wait_time = 3.0
+			rpc("_cycle_entrance")
+			cycle_timer.start()
 		_cycles.countdown:
-			pass
+			printg(["countdown"])
+			rpc("_cycle_countdown") # cycle timer is called within _cycle countdown
 		_cycles.start:
-			pass
+			printg(["start"])
+			cycle_timer.start()
+			rpc("_cycle_start")
 		_cycles.game:
-			pass
+			printg(["game"])
+			rpc("_cycle_game")
+			cycle_timer.wait_time = play_time
+			cycle_timer.stop()
+			cycle_timer.start()
 		_cycles.stop:
+			printg(["stop"])
+			rpc("_cycle_stop")
+			cycle_timer.wait_time = result_time
 			results = _get_results()
+			cycle_timer.start()
 		_cycles.end:
-			pass
+			rpc("_cycle_end")
+			printg(["end"])
+			cycle_timer.start()
 		_cycles.exit:
-			pass
-	Party.end_game(player_scores)
+			rpc("_cycle_exit")
+			printg(["exit"])
+			ended = true
+			Party.end_game(player_scores)
+	_cycle_var+=1
+
+onready var cycle_timer : Timer = Timer.new()
+
+func _setup_cycle_timer():
+	self.add_child(cycle_timer)
+	cycle_timer.one_shot = true
+	catch_connection(cycle_timer.connect("timeout",self,"_cycle"))
+
+remotesync func _cycle_entrance():
+	#target_pos = end_pos
+	pass
+
+remotesync func _cycle_countdown():
+	_countdown() # esta función hará un llamado que cambiará el ciclo !
+
+remotesync func _cycle_start():
+	_start()
+
+remotesync func _cycle_game():
+	_game()
+
+remotesync func _cycle_stop():
+	_stop()
+	pass
+
+remotesync func _cycle_end():
+	_end()
+	pass
+
+remotesync func _cycle_exit():
+	_exit()
+	pass
+
+
+################# UI Overlay behaviour ################
+
+onready var countdown_timer : Timer = Timer.new()
+
+export(int,2,10) var countdown_time : int = 5
+export(float,0.1,10) var countdown_time_step : float = 0.9
+onready var _remaining_countdown : int = countdown_time
+onready var countdown_label :RichTextLabel = $"UI Overlay/Contenedor/Countdown"
+var _countdown_timer_setup : bool = false
+
+func _countdown():
+	if !_countdown_timer_setup:
+		_setup_countdown_timer()
+	
+	if _remaining_countdown>0:
+		countdown_label.bbcode_text =(
+			"[center][rainbow]%d[/rainbow][/center]"%_remaining_countdown
+			)
+	else:
+		countdown_label.bbcode_text = "[center][rainbow]START[/rainbow][/center]"
+	countdown_label.show()
+	if _remaining_countdown>=0:
+		countdown_timer.start()
+	_remaining_countdown -= 1
+	if is_network_master() and _remaining_countdown<0:
+		_cycle()
+
+func _setup_countdown_timer():
+	self.add_child(countdown_timer)
+	countdown_timer.wait_time = countdown_time_step
+	countdown_timer.one_shot = true
+	countdown_timer.connect("timeout",self,"_countdown")
+	_countdown_timer_setup=true
+
+func _start():
+	emit_signal("unfreeze")
+	countdown_label.hide()
+
+func _game():
+	pass
+
+func _stop():
+	emit_signal("freeze")
+	pass
+
+func _end():
+	pass
+
+func _exit():
+	pass
+
+###################################################
 
 # returns a list of results and updates player_scores
 func _get_results() -> Array:
@@ -122,7 +258,7 @@ func _get_results() -> Array:
 	for child in player_list:
 		var p = child.player
 		var big = child.biggyness
-		if big==second_best_player:
+		if big==second_biggest:
 			second_draw.push_back(p)
 	
 	# get everyone else:
@@ -144,7 +280,6 @@ func _draw(firsts,greatest):
 		else:
 			player_scores.push_back({"player":p,"points":simp_scores})
 	return
-
 
 # writes player scores
 export var simp_scores : int = 10
@@ -173,3 +308,17 @@ func _loser_score_function(strength,greatest):
 
 func _draw_score_function(greatest):
 	return min(-20,-int(50*(greatest)/reasonable_key_presses))
+
+## utilities
+
+func printg(args):
+	print("Bonk: ")
+	for arg in args:
+		print(arg)
+
+func catch_connection(variant,origin : String = ""):
+	if variant!=OK:
+		if origin=="":
+			printg(["error!"])
+		else:
+			printg(["error on ",origin])
